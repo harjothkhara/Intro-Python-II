5da8fbdbb7e1e4d5b83ae2fff908da99bbf31e05
harjoth <harjoth.khara@gmail.com>
2019-10-03 14:31:53 -0700

    added methods in item that identify what a player has picked up and dropped.

--- src/item.py
+++ src/item.py
@@ -2,3 +2,9 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def on__take(self):
+        print(f"You have picked up: {self.name}")
+
+    def on__drop(self):
+        print(f"You have dropped: {self.name}")
